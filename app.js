9ddad891f3e2ec389309d9e7944851e5e30e1987
Sergey Zaligin <webdevjsphp@gmail.com>
2018-11-10 14:41:19 +0300

    fix

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const mysql = require('mysql');
 const authRoutes = require('./routes/auth');
 
 const config = require('./config/keys');
 
 const app = express();
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'cobweb',
+  password: '123456',
+  database: 'api',
+});
+
+pool.query('SELECT * FROM user', (error, results, fields) => {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
